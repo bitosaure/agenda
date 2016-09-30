4e58eab40f64499ae3e057768baae9a26740703c
Thibault <thibaultlg@gmail.com>
2016-09-30 16:40:01 +0200

    Ajouts dépendances dans file.js

--- agenda/files.js
+++ agenda/files.js
@@ -8,6 +8,15 @@ modelFiles = {
         'src/modules/forms/module.js',
         'src/modules/forms/{,**/}*.js',
 
+        'src/modules/calendrier/module.js',
+        'src/modules/calendrier/{,**/}*.js',
+
+        'src/modules/notification/module.js',
+        'src/modules/notification/{,**/}*.js',
+
+        'src/modules/event/module.js',
+        'src/modules/event/{,**/}*.js',
+
 
         'src/module.js',
         'src/services/{,**/}*.js'
