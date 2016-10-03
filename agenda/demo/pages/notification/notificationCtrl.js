a9d7bee5f4cb8416b276e4ea5113f69762b65309
florianbnd <florianbesnard@yahoo.com>
2016-10-03 16:11:20 +0200

    Commit Notification #4

--- agenda/demo/pages/notification/notificationCtrl.js
+++ agenda/demo/pages/notification/notificationCtrl.js
@@ -13,6 +13,7 @@ angular.module('demoApp')
         $scope.showCode = function(){
             $scope.displayCode = !$scope.displayCode;
         };
+        
 
         /**
          * Page description
