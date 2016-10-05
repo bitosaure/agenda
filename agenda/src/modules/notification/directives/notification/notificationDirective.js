3baf6e56a7e3f32ebfe10b47714118fb2634180b
florianbnd <florianbesnard@yahoo.com>
2016-10-05 22:24:44 +0200

    Commit Notification #5 (Ajout de spoilers pour masquer/afficher les notifications)

--- agenda/src/modules/notification/directives/notification/notificationDirective.js
+++ agenda/src/modules/notification/directives/notification/notificationDirective.js
@@ -8,6 +8,26 @@ angular.module('eklabs.angularStarterPack.notification')
 
             },link : function(scope){
 
+                scope.showUnreaded = true;
+                scope.showReaded = false;
+
+                scope.changeVisibility = function(b) {
+
+                    //Si b est égal à true, on se focalise sur les notifications non vue
+                    if(b){
+                        if(scope.showUnreaded)
+                            scope.showUnreaded = false;
+                        else
+                            scope.showUnreaded = true;
+                    }else{
+                        if(scope.showReaded)
+                            scope.showReaded = false;
+                        else
+                            scope.showReaded = true;
+                    }
+
+                };
+
                 scope.notifications = [
                     {
                         "sender" : "Bitosaur",
