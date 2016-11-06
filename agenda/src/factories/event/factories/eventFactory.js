084ffd0e7f21b9be2492fd3d894852c6ffcbe7fe
System Administrator <Lphamike>
2016-11-06 15:38:14 +0100

    Factory Event crée

--- agenda/src/factories/event/factories/eventFactory.js
+++ agenda/src/factories/event/factories/eventFactory.js
@@ -1,3 +1,17 @@
-/**
- * Created by maximer on 06/11/16.
- */
+'use strict';
+
+angular.module('eklabs.angularStarterPack.event')
+    .factory('event', function(eventObj) {
+    return {
+        "name": eventObj.name,
+        "image" : eventObj.image,
+        "description" : eventObj.description,
+        "location" : eventObj.location,
+        "startDate" : eventObj.startDate,
+        "endDate" : eventObj.endDate,
+        "organizer" : eventObj.organizer,
+        "eventStatus" : eventObj.eventStatus,
+        "attendee" : eventObj.attendee,
+        "visibility" : eventObj.visibility
+    }
+});
