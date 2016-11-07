996d4677fca1acd65499e68abd71aeb622931d1f
System Administrator <Lphamike>
2016-11-07 11:57:23 +0100

    Ajout du service event

---
+++ agenda/src/services/event/services.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('eklabs.angularStarterPack.event')
+    .service("eventService", function(params){
+        this.createEvent = function(params){};
+        this.updateEvent = function(params){};
+        this.deleteEvent = function(params){};
+        this.addParticipant = function(params){};
+        this.deleteParticipant = function(params){};
+        this.joinEvent = function(params){};
+        this.unsubscribe = function(params){};
+        this.acceptInvitation = function(params){};
+        this.refuseInvitation = function(params){};
+        this.getEvent = function(params){};
+});
