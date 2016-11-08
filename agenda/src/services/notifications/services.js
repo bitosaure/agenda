9111f70a32538ee12d88a5dde4d458a6b30e09ef
florianbnd <florianbesnard@yahoo.com>
2016-11-08 14:01:27 +0100

    Modification nom de la méthode loadNotifications par getNotifications
    Implémentation de la méthode readNotifification(id)

--- agenda/src/services/notifications/services.js
+++ agenda/src/services/notifications/services.js
@@ -3,13 +3,22 @@
 angular.module('demoApp')
     .service('notification', function() {
 
-        this.loadNotifications = function () {
-
+        this.getNotifications = function () {
             $http.get('91.134.241.60:3080/ressources/Event/notification').then(function(response){
                 return response.data;
             });
+        };
 
-        }
+        this.readNotification = function(idNotif) {
+            $http.post('91.134.241.60:3080/ressources/Event/notification/'+idNotif).then(function(response){
+                return response.data;
+            });
+        };
+
+
+
+
+
 
 
     });
