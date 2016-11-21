14e39320423f66a778bafe5d0e6ff31600c9b09c
Eric Madeva <eddie.madeva77@hotmail.fr>
2016-11-21 15:10:39 +0100

    Mise a jour de la directive event

--- agenda/src/modules/event/directives/event/eventDirective.js
+++ agenda/src/modules/event/directives/event/eventDirective.js
@@ -8,6 +8,27 @@ angular.module('eklabs.angularStarterPack.event')
                 user        : '=',
                 callback    : '=?'
             },link : function(scope){
+                scope.case = 1;
+                scope.update_event = false;
+                scope.event = {
+                    name : "BBQ chez Eddie",
+                    startDate : "30 Septembre 2016",
+                    endDate : "1 Octobre 2016",
+                    description : "Big Barbecue chez Eddie",
+                    organizer : "Eddie MADEVA",
+                    location : "38 rue Pasteur, 77240 Vert-Saint-Denis",
+                    attendees : [
+                        "Eddie MADEVA",
+                        "Thibault LE GRAND",
+                        "Emilie PISU",
+                        "Florian BESNARD"
+                    ]
+                }
+
+                scope.openEvent = function(event_id){
+                    console.log(event_id);
+                    scope.case = 1;
+                };
 
                 /**
                  * 
