f440bde66c231a58b6bf0bac03d0fa960d75a977
Eddie MADEVA <eddie.madeva77@hotmail.fr>
2016-12-13 10:29:41 +0100

    Résolution des conflits

--- agenda/demo/pages/calendrier/calendrierCtrl.js
+++ agenda/demo/pages/calendrier/calendrierCtrl.js
@@ -47,7 +47,7 @@ angular.module('demoApp')
                         {title: 'Birthday Party',start: new Date(2016, 11, 25, 19, 0),end: new Date(2016, 11, 25, 22, 30),allDay: false},
                         {title: 'Click for Google',start: new Date(2016, 11, 28,15,0),end: new Date(2016, 11, 29,15,0),url: 'http://google.com/'}
                     ]
-                
+
             ],
             //json       : {"hello" : "world"},
             callback   : undefined,
@@ -61,26 +61,14 @@ angular.module('demoApp')
             options    : undefined,
             json       : undefined,
             eventSources: [
-<<<<<<< HEAD
-                {events: [
 
-=======
-                { events :
                     [
->>>>>>> parent of 079a365... ajout de la factory, modification du service et du controleur
                         {id: 999,title: 'Repeating Event',start: new Date(2016, 11, 17 - 3, 16, 0),allDay: false},
                         {id: 999,title: 'Repeating Event',start: new Date(2016,11, 24, 16, 0),allDay: false},
                         {title: 'Birthday Party',start: new Date(2016, 11, 25, 19, 0),end: new Date(2016, 11, 25, 22, 30),allDay: false},
                         {title: 'Click for Google',start: new Date(2016, 11, 28,15,0),end: new Date(2016, 11, 29,15,0),url: 'http://google.com/'}
-<<<<<<< HEAD
-
-                ],
-                    color: 'black',
-                    textColor: 'yellow'
-=======
                     ]
->>>>>>> parent of 079a365... ajout de la factory, modification du service et du controleur
-                }
+                
             ],
             callback   : {
                 valid : function(json){
@@ -107,7 +95,7 @@ angular.module('demoApp')
             $scope.refresh        = moment().valueOf();
             $scope.haveResult     = false;
 
-            console.log("toto "+$scope.events);
+            console.log($scope.events);
         };
 
         // --- Init
