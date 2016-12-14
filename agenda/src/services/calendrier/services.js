77e97b65d38772fff82c7e94d486f6c5e46f244e
Thibault <thibaultlg@gmail.com>
2016-12-14 09:28:53 +0100

    modifs

--- agenda/src/services/calendrier/services.js
+++ agenda/src/services/calendrier/services.js
@@ -2,36 +2,31 @@
  * Created by emilie on 08/11/2016.
  */
 angular.module('eklabs.angularStarterPack.calendrier')
-<<<<<<< HEAD
-    .service("calendrierService", function($http, $config, calendrierFactory){
+
+    .service("calendrierService", function($http, $config, calendrierFactory) {
         console.log($config.getEventBaseUrl());
 
-        function successCallback (response){
+        function successCallback(response) {
             console.log(response);
             return response;
         };
 
-        function errorCallback(response){
+        function errorCallback(response) {
             console.log(response);
             return {};
         };
-        this.getEventsCalendar = function(){
+        this.getEventsCalendar = function () {
             return $http.get($config.getEventBaseUrl() + 'event/', $config).then(
-                function(response){
+                function (response) {
                     var eventsCalendar = [];
-                    angular.forEach(response.data, function(value){
+                    angular.forEach(response.data, function (value) {
                         eventsCalendar.push(new calendrierFactory(value));
                     });
                     return eventsCalendar;
                 },
-                function(response){
+                function (response) {
                     return errorCallback(response);
                 });
         };
-=======
-    .service("calendrierService", function(params){
->>>>>>> parent of 079a365... ajout de la factory, modification du service et du controleur
-
-        this.getEvents = function(params){};
-    });
+    }
     
