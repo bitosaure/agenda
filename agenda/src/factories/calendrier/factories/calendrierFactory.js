58563b149eff0bdbab1a271cf91c6210c8980241
emipisu <emilie.pisu@gmail.com>
2016-12-16 13:49:52 +0100

    modification de la factory

--- agenda/src/factories/calendrier/factories/calendrierFactory.js
+++ agenda/src/factories/calendrier/factories/calendrierFactory.js
@@ -12,16 +12,16 @@ angular.module('eklabs.angularStarterPack.calendrier')
          function calendrierFactory(eventObj){
             this.id = eventObj.id;
             this.title = eventObj.name;
-            //this.image = eventObj.image
-             //this.description = eventObj.description;
-            //this.location = eventObj.location;
+            this.image = eventObj.image
+            this.description = eventObj.description;
+            this.location = eventObj.location;
             this.start = new Date(eventObj.startDate);
             this.end =new Date(eventObj.endDate);
              
-            //this.organizer = eventObj.organizer;
-            //this.eventStatus = eventObj.eventStatus;
-           // this.attendee = eventObj.attendee;
-           // this.visibility = eventObj.visibility;
+            this.organizer = eventObj.organizer;
+            this.eventStatus = eventObj.eventStatus;
+            this.attendee = eventObj.attendee;
+            this.visibility = eventObj.visibility;
             this.allDay=false;
 
 
